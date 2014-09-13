1fcee678cc8103bc8dd89f3a738821481590e92f
Ari Russo <ari.russo@gmail.com>
2014-09-12 20:59:28 -0400

    comments, remove extraneous methods

--- lib/sequencer/core.rb
+++ lib/sequencer/core.rb
@@ -1,5 +1,6 @@
 module Sequencer 
 
+  # The core sequencer
   class Core
 
     attr_reader :event, :trigger, :state
@@ -8,43 +9,51 @@ module Sequencer
       @event = Event.new
       @trigger = EventTrigger.new
       @state = State.new
+      @running = false
     end
 
-    # toggle start/stop
+    # Toggle start/stop
+    # @return [Boolean]
     def toggle_start
-      running? ? stop : start
+      @state.running? ? stop : start
     end
 
-    def running=(val)
-      val ? start : stop
-    end
-
-    # start the clock
-    def start(options = {}, &block)     
+    # Mark state as running and trigger start event
+    # @return [Boolean]
+    def start    
       @state.start
       @event.do_start(@state)
-      yield if block_given?
       true
     end
 
-    # Stops the clock and sends any remaining MIDI note-off messages that are in the queue
-    def stop(options = {}, &block)
+    # Mark state as not running and trigger stop event
+    # @return [Boolean]
+    def stop
       @state.stop
       @event.do_stop(@state)
-      yield if block_given?
       true            
     end
 
+    # Execute a single cycle of sequencing (perform and step)
+    # @param [Array] sequence The sequence to execute a single cycle of
+    # @return [Boolean] Whether perform and step both finished
     def exec(sequence)
       perform(sequence) && step(sequence)
     end
 
+    # Step the sequencer and fire the step event
+    # @param [Array] sequence
+    # @return [Boolean]
     def step(sequence)
       @state.step(sequence.length)
       @event.do_step(@state)
       true
     end
 
+    # If a stop is triggered, stop. Otherwise if reset is triggered, reset. Finally,
+    # fire the perform event on the current step of the sequence
+    # @param [Array] sequence
+    # @return [Boolean] True if perform event was fired
     def perform(sequence)
       data = sequence.at(@state.pointer)
       unless data.nil?

8f18f3d27abebd9542bb287a81ed7e0903ac8c27
Ari Russo <ari.russo@gmail.com>
2014-09-12 20:50:39 -0400

    template for full test

--- test/core_test.rb
+++ test/core_test.rb
@@ -1,26 +1,61 @@
 require "helper"
 
 class Sequencer::CoreTest < Test::Unit::TestCase
-  
-  context "Sequencer" do
+
+  context "Core" do
 
     setup do
-      @sequence = [1,2,3,4]
-      @cache = []
-      sequencer = Sequencer.new
-      clock = Sequencer::Clock.new(120)
-      clock.event.tick { sequencer.exec(@sequence) }
-      sequencer.event.perform { |state, data| @cache << data }
-      sequencer.trigger.stop { |state| state.repeat == 1 }
-      sequencer.event.stop { clock.stop }
-      clock.start(:focus => true)
+      @sequencer = Sequencer::Core.new
+      @clock = Sequencer::Clock.new(120)
+      @clock.event.tick { @sequencer.exec(@sequence) }
+      @sequencer.event.stop { @clock.stop }
     end
 
-    should "create cache of sequence" do
-      assert_equal @sequence, @cache
+    context "#toggle_start" do
+      
+    end
+
+    context "#start" do
+
+    end
+
+    context "#stop" do
+
+    end
+
+    context "#running=" do
+
+    end
+
+    context "#exec" do
+
+    end
+
+    context "#step" do
+
+    end
+
+    context "#perform" do
+
+    end
+
+    context "Functional" do
+
+      setup do
+        @sequence = [1,2,3,4]
+        @cache = []
+        @sequencer.event.perform { |state, data| @cache << data }
+        @sequencer.trigger.stop { |state| state.repeat == 1 }
+        @clock.start(:focus => true)
+      end
+
+      should "create cache of sequence" do
+        assert_equal @sequence, @cache
+      end
+
     end
 
   end
-    
+
 end
 

09e8556909aa4ac7f48d40abecae0442a8e58eb5
Ari Russo <ari.russo@gmail.com>
2014-09-18 19:48:03 -0700

    test adding and removing outputs

--- test/clock_test.rb
+++ test/clock_test.rb
@@ -8,6 +8,46 @@ class Sequencer::ClockTest < Test::Unit::TestCase
       @clock = Sequencer::Clock.new(120)
     end
 
+    context "Clock#initialize" do
+
+      should "get MIDI output" do
+        output = Object.new
+        @clock = Sequencer::Clock.new(120, :output => output)
+        assert_not_nil @clock.midi_outputs
+        assert_not_empty @clock.midi_outputs
+        assert @clock.midi_outputs.include?(output)
+      end
+
+    end
+
+    context "Clock#add_midi_output" do
+
+      should "add MIDI output" do
+        output = Object.new
+        refute @clock.midi_outputs.include?(output)
+        @clock.add_midi_output(output)
+        assert_not_nil @clock.midi_outputs
+        assert_not_empty @clock.midi_outputs
+        assert @clock.midi_outputs.include?(output)
+      end
+
+    end
+
+    context "Clock#remove_midi_output" do
+
+      should "remove MIDI output" do
+        output = Object.new
+        refute @clock.midi_outputs.include?(output)
+        @clock.add_midi_output(output)
+        assert_not_nil @clock.midi_outputs
+        assert_not_empty @clock.midi_outputs
+        assert @clock.midi_outputs.include?(output)
+        @clock.remove_midi_output(output)
+        refute @clock.midi_outputs.include?(output)
+      end
+
+    end
+
     context "Clock#tempo" do
 
       should "get tempo" do

1f47f0a8a1057e3fc62161bbb18830d14e2d060e
Ari Russo <ari.russo@gmail.com>
2015-01-30 19:50:54 -0500

    version bump

--- lib/sequencer.rb
+++ lib/sequencer.rb
@@ -4,7 +4,7 @@
 #
 # (c)2011-2014 Ari Russo
 # Licensed under Apache 2.0
-# 
+#
 
 # libs
 require "forwardable"
@@ -18,7 +18,7 @@ require "sequencer/event_trigger"
 require "sequencer/loop"
 
 module Sequencer
-  
-  VERSION = "0.0.10"
-  
+
+  VERSION = "0.0.11"
+
 end
